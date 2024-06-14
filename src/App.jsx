6ccd55dc9c63ec4bd0b18c937df5945f3b052f40
MubeenShaikh123 <mubeenshaikh8072@gmail.com>
2024-06-14 17:43:27 +0530

    Added debouncing in search

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
 import Weather from './components/Weather';
 import Form from './components/Form';
-import { useState,useEffect } from 'react';
+import { useState,useEffect, useRef } from 'react';
 
 
 function App() {
-
-  const [search, setSearch] = useState('Mumbai');
   const [city, setCity] = useState(null);
   const [error, setError] = useState(true);
   const [unit, setUnit] = useState("degree");
+  const timeout=useRef(null)
+  const fetchData = async (searchQuery) => {
+    console.log("fetch called")
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${import.meta.env.VITE_API_KEY}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setError(null)
+      setCity(data)
+
+    } catch (error) {
+      setError(error)
+    }
+  };
+
+  const debounceSearch=(searchQuery)=>{
+    clearTimeout(timeout.current);
+    timeout.current=setTimeout(()=>{
+      fetchData(searchQuery);
+    },1000)
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${import.meta.env.VITE_API_KEY}`
-        );
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setError(null)
-        setCity(data)
-
-      } catch (error) {
-        setError(error)
-        console.error('Error fetching weather data:', error);
-      }
-    };
-
-    fetchData();
-  }, [search]);
+    fetchData('mumbai');
+  },[]);
 
   return (
     <div className="main container-fluid">
-      <Form setSearch={setSearch}></Form>
+      <Form debounceSearch={debounceSearch}></Form>
       <Weather setUnit={setUnit} error={error} city={city} unit={unit}></Weather>
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,11 @@
 export default function Form(props) {
-    const setSearch= props.setSearch
+    const debounceSearch= props.debounceSearch
     return (
         <form className='pt-3 pb-2 '>
             <div className="container">
                 <div className="d-flex justify-content-center ">
                     <input className="searchInput" placeholder="Enter City Name" type="text" onChange={(e) => {
-                            setSearch(e.target.value)
+                            debounceSearch(e.target.value)
                         }} />
                 </div>
             </div>
